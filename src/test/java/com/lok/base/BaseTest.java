203e100ce2670213eecb514134daf3d1ab266e47
lokeshghundiyal <lokeshghundiyal@gmail.com>
2024-06-23 18:57:10 +0530

    dkd

---
+++ src/test/java/com/lok/base/BaseTest.java
@@ -0,0 +1,12 @@
+package com.lok.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+}
